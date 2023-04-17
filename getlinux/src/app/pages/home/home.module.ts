597ff4aec5823718d10ad77dc8d973d3ce92ac29
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-17 12:20:47 +0200

    Home page has linux logos

--- getlinux/src/app/pages/home/home.module.ts
+++ getlinux/src/app/pages/home/home.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
-
+import { LogocardComponent } from 'src/app/components/home/logocard/logocard.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    HomeRoutingModule
-  ]
+  declarations: [HomeComponent, LogocardComponent],
+  imports: [CommonModule, HomeRoutingModule],
 })
-export class HomeModule { }
+export class HomeModule {}
