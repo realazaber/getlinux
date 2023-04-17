82feda435149859390df3908e76270a05ad50791
swagoverlord <azcooldude8@gmail.com>
2023-04-17 10:21:03 +0200

    base home page

--- getlinux/src/app/pages/home/home.component.ts
+++ getlinux/src/app/pages/home/home.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  @Input() title: String = '...';
+  @Input() pgraph: String = '...';
 
+  @Input() featuredImg: String = '...';
+  @Input() featuredImgAlt: String = '...';
 }
