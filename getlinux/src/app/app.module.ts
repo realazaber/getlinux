bced83ce27367b87320aab80bafba171691226a5
therealcoolpup <47402666+therealcoolpup@users.noreply.github.com>
2023-04-17 11:56:10 +0200

    Update app.module.ts

--- getlinux/src/app/app.module.ts
+++ getlinux/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LogocardComponent } from './components/home/logocard/logocard.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, FooterComponent],
+  declarations: [AppComponent, NavComponent, FooterComponent, LogocardComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
