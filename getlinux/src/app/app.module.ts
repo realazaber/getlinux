1a37f8b6a253b24fb1e1ff1409f42a693496a598
swagoverlord <azcooldude8@gmail.com>
2023-04-18 17:26:04 +0200

    Update app.module.ts

--- getlinux/src/app/app.module.ts
+++ getlinux/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [AppComponent, NavComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
