c3c0745260d8842efa59e8b5f5ac67f1058e8300
swagoverlord <azcooldude8@gmail.com>
2023-04-18 17:48:54 +0200

    About page works

--- getlinux/src/app/pages/about/about.component.ts
+++ getlinux/src/app/pages/about/about.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { PageDataService } from 'src/app/services/page-data.service';
+import { Page } from 'src/app/interfaces/Page';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
+  pageData: Page | null = null;
 
+  constructor(private pageDataService: PageDataService) {}
+
+  ngOnInit() {
+    this.pageDataService
+      .getPage('684e2f6e-5238-4b24-9079-2ab1f2b3ea02')
+      .subscribe((page) => {
+        this.pageData = page;
+        console.log('component', this.pageData);
+      });
+  }
 }
