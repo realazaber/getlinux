47b6489345f6d89b38eda572a1b837c3d1aa7e8b
swagoverlord <azcooldude8@gmail.com>
2023-04-18 17:25:53 +0200

    Service for getting page data

--- getlinux/src/app/services/page-data.service.ts
+++ getlinux/src/app/services/page-data.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { of, forkJoin } from 'rxjs';
+
 import { Page } from '../interfaces/Page';
 
 @Injectable({
@@ -8,13 +12,43 @@ import { Page } from '../interfaces/Page';
 export class PageDataService {
   constructor(private http: HttpClient) {}
 
-  baseUrl: String = 'https://backend-getlinux.azaber.com/jsonapi/node/';
+  baseUrl: string = 'https://backend-getlinux.azaber.com/jsonapi/node/';
 
-  getPage(pageId: Number): Page {
-    //Make request to get page title, pgraph, featuredimgalt
-    // https://backend-getlinux.azaber.com/jsonapi/node/page/9258053c-6866-4b0f-be50-677e2ce363a3
-    //Make request to get featuredimgurl
-    // https://backend-getlinux.azaber.com/jsonapi/node/page/9258053c-6866-4b0f-be50-677e2ce363a3/field_bp_featured_image
-    //Return page
+  getPage(pageId: string): Observable<Page> {
+    const url = this.baseUrl + 'page/' + pageId;
+
+    return this.http.get<any>(url).pipe(
+      switchMap((response) => {
+        const featuredImgUrl$ = response.data.relationships
+          .field_bp_featured_image
+          ? this.http.get<any>(
+              response.data.relationships.field_bp_featured_image.links.related
+                .href
+            )
+          : of(null);
+
+        return forkJoin([of(response), featuredImgUrl$]);
+      }),
+      map(([response, featuredImgResponse]) => {
+        const pageData = response.data.attributes;
+        const featuredImgData = featuredImgResponse
+          ? featuredImgResponse.data.attributes.uri.url
+          : null;
+        console.log('featuredimgdata', featuredImgData);
+
+        const page: Page = {
+          id: pageData['drupal_internal__nid'],
+          title: pageData['title'],
+          pgraph: pageData['body'].processed,
+          featuredImgAlt:
+            pageData['field_bp_featured_image']?.data?.meta?.alt || '',
+          featuredImg: 'https://backend-getlinux.azaber.com' + featuredImgData,
+        };
+
+        console.log('service', page);
+
+        return page;
+      })
+    );
   }
 }
