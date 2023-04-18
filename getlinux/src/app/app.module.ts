7c84bbf7fd3c1782551c5ba7614898fc11687570
swagoverlord <azcooldude8@gmail.com>
2023-04-18 09:46:43 +0200

    Update app.module.ts

--- getlinux/src/app/app.module.ts
+++ getlinux/src/app/app.module.ts
@@ -5,10 +5,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LogocardComponent } from './components/home/logocard/logocard.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, FooterComponent, LogocardComponent],
+  declarations: [AppComponent, NavComponent, FooterComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
